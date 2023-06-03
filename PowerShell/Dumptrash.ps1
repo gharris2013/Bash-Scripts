0401aa48aa4ec92523b9845a6302187d2b4563ef
gharris2013 <g_harris2013@yahoo.com>
2023-06-03 01:43:42 -0500

    Moved other Repos

--- PowerShell/Dumptrash.ps1
+++
@@ -1,5 +0,0 @@
-$shell = New-Object -ComObject Shell.Application
-$recycleBin = $shell.Namespace(0xA)
-$recycleBin.Items() | foreach {
-    $recycleBin.InvokeVerb("Delete")
-}
--- temp/Pingscan.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Enter the Subnet"
-read SUBNET
-for IP in $(seq 1-254); do
-    ping -c 1 $SUBNET.$IP
-fi
-0
